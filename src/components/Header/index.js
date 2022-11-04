2a97a6242d0c581df4e10be0d0126892449bac5d
Anoop Chauhan <chauhananu0567@gmail.com>
2022-11-04 20:54:31 +0530

    antd setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <Button type="primary">Button</Button>
+    </header>
+  );
+}
+
+export default Header;
