92dce359e1abc1225d3cf57832f4d54e820ec640
Anoop Chauhan <chauhananu0567@gmail.com>
2022-11-06 19:51:07 +0530

    graphql

--- src/components/MoviesContent/index.js
+++ src/components/MoviesContent/index.js
@@ -1,7 +1,30 @@
 import MovieCard from "../MovieCard";
 import { FILMS } from "../../db/films";
+import { useQuery, gql } from "@apollo/client";
+
+const FILMS_QUERY = gql`
+  {
+    allFilms {
+      films {
+        id
+        title
+        releaseDate
+        producers
+        director
+        openingCrawl
+      }
+    }
+  }
+`;
 
 const MoviesContent = () => {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+
+  console.log("data", data);
+
   return (
     <div className="container">
       <div className="row my-3">
