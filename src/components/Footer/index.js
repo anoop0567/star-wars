0f8f140df88d6386d1e23ba4e703fc7e9808662a
Anoop Chauhan <chauhananu0567@gmail.com>
2022-11-06 22:41:58 +0530

    footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="container">
+      <footer class="py-3 my-4">
+        <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
